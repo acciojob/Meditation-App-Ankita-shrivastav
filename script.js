75af025d967826abbd1d02def3627255abc3d76f
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-27 01:04:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const vidContainer=document.querySelector('.vide-container');
+const playerContainer=document.querySelector('.player-container');
+const soundAButton=document.querySelector('#sound-a');
+const soundBButton=document.querySelector('#sound-b');
+const smallerMinsButton=document.querySelector('#smaller-mins');
+const mediumMinsButton=document.querySelector('#medium-mins');
+const longMinsButton=document.querySelector('#long-mins');
+const timeDisplay=document.querySelector('.time-display');
+const playButton=document.querySelector('.play');
+
+soundAbutton.addEventListener('click',playSoundA);
+soundBbutton.addEventListener('click',playSoundB);
+smallerMinsButton.addEventListener('click',setTimeTo2Mins);
+mediumMinsButton.addEventListener('click',setTimeTo5Mins);
+longMinsButton.addEventListener('click',setTimeTo10Mins);
+playButton.addEventListener('click',togglePlay);
+
+function playSoundA(){
+	const audio=new Audio('');
+	audio.play();
+}
